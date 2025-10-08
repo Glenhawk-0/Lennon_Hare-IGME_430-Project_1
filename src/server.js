3c323aebb1297277d1e6e74736a77219007ca7dd
Lennon-0 <LennonEhare@Gmail.com>
2025-10-08 14:47:59 -0400

    getting there

--- src/server.js
+++ src/server.js
@@ -2,13 +2,55 @@
 // ill be honest though, i like... kinda half understand how it works.
 // i apologize.
 const http = require('http');
+const query = require('querystring');
 const htmlHandler = require('./htmlResponses.js');
 const mediaHandler = require('./mediaResponses.js');
-
 const jsonHandler = require('./jsonResponses.js');
 
 // set the port. process.env.PORT and NODE_PORT are for servers like heroku
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+
+//parseBody
+const parseBody = (request, response, handler) => {
+  // The request will come in in pieces. We will store those pieces in this
+  // body array.
+  const body = [];
+
+  // The body reassembly process is event driven, much like when we are streaming
+  // media like videos, etc. We will set up a few event handlers. This first one
+  // is for if there is an error. If there is, write it to the console and send
+  // back a 400-Bad Request error to the client.
+  request.on('error', (err) => {
+    console.dir(err);
+    response.statusCode = 400;
+    response.end();
+  });
+
+  // The second possible event is the "data" event. This gets fired when we
+  // get a piece (or "chunk") of the body. Each time we do, we will put it in
+  // the array. We will always recieve these chunks in the correct order.
+  request.on('data', (chunk) => {
+    body.push(chunk);
+  });
+
+  // The final event is when the request is finished sending and we have recieved
+  // all of the information. When the request "ends", we can proceed. Turn the body
+  // array into a single entity using Buffer.concat, then turn that into a string.
+  // With that string, we can use the querystring library to turn it into an object
+  // stored in bodyParams. We can do this because we know that the client sends
+  // us data in X-WWW-FORM-URLENCODED format. If it was in JSON we could use JSON.parse.
+  request.on('end', () => {
+    const bodyString = Buffer.concat(body).toString();
+    request.body = query.parse(bodyString);
+
+    // Once we have the bodyParams object, we will call the handler function. We then
+    // proceed much like we would with a GET request.
+    handler(request, response);
+  });
+};
+
+
 
 // key:value object to look up URL routes to specific functions
 const urlStruct = {
@@ -26,9 +68,13 @@ const urlStruct = {
   // aparently it need some kind of parse body thinng and i have no idea
   // what that is and im not gonna just copy paste AI
   // im going to just have place holder things for now.
-  '/api/books/add': jsonHandler.getNewBook,
 
-  '/api/books/update': jsonHandler.getUpdateBook,
+    '/api/books/add': (request, response) => { 
+    parseBody(request, response, jsonHandler.getNewBook);
+    },
+    '/api/books/update': (request, response) => {
+    parseBody(request, response, jsonHandler.getUpdateBook);
+    },
   //--
   notFound: jsonHandler.getNotFound, // default
 };
